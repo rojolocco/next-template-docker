16157841f2e5a089dc7dd8fa698e05248654a603
rojolocco <rojolocco@gmail.com>
2025-07-04 12:25:27 -0500

    Zustand added

---
+++ src/stores/use-store-main.ts
@@ -0,0 +1,14 @@
+// Test
+import { create } from "zustand";
+
+type AppState = {
+    count: number
+    increase: () => void
+    reset: () => void
+}
+
+export const useAppStore = create<AppState>((set) => ({
+    count: 0,
+    increase: () => set((state) => ({ count: state.count + 1 })),
+    reset: () => set({ count: 0 }),
+}));
