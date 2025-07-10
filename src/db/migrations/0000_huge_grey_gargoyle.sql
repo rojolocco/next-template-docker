2d81ff45b9cf9950f195d3b01c53859c5385fff8
rojolocco <rojolocco@gmail.com>
2025-07-09 21:46:42 -0500

    Repo Cleaned

--- src/db/migrations/0000_huge_grey_gargoyle.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "example" (
-	"id" integer PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"email" varchar(255),
-	"is_active" boolean DEFAULT true,
-	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP
-);
--- src/db/schemas/index.ts
+++ src/db/schemas/index.ts
@@ -1,17 +1,1 @@
 // Export all schemas from this directory
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-export { default as exampleTable } from "./dynamic-tables";
->>>>>>> ec0ecf8 (format changes)
-=======
->>>>>>> 041c4217038713ad335995a03c4a301a90fe9f90
-=======
-export { default as exampleTable } from "./dynamic-tables";
->>>>>>> ec0ecf8 (format changes)
-=======
-export { default as exampleTable } from "./dynamic-tables";
-=======
->>>>>>> dev2
->>>>>>> 7da3108aa63704d412650c657c16daf24e7ba0bf
