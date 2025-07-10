a53948d9016535f49dcbda7afcda3a717dfbf43d
rojolocco <rojolocco@gmail.com>
2025-07-09 21:01:33 -0500

    supabase client updated [skip ci]

--- src/lib/supabase-client.ts
+++ src/lib/supabase-client.ts
@@ -3,4 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseAnonKey = process.env.SUPABASE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+export default supabase;
